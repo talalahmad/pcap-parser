769484727b3166396b125219e5ebb96d14b2bd4f
talalahmad <mailtalalahmad@gmail.com>
2018-10-30 18:13:55 -0400

    Hint added to match dns query to response

--- dns-parser.py
+++ dns-parser.py
@@ -1,20 +1,7 @@
-#!/usr/bin/python
-
 import sys
 from subprocess import Popen, PIPE
 
-eventlist = []
-activeports = {}
-dnsqueries = {}
-"""
-try:
-    f = open(sys.argv[1])
 
-except:
-    print "Please specify an input file"
-
-line = f.readline()
-"""
 #-e dns.flags.rcode
 cmd = 'tshark -r ' + sys.argv[1] +' -2 -R "dns" -T fields -e frame.time_relative -e ip.src -e ip.dst -e dns.qry.name -e _ws.col.Info'
 
@@ -27,3 +14,11 @@ lines = lines.split('\n')
 
 for line in lines:
 	print line
+	#this line seperates line into columns
+	columns = line.split("\t")
+	time = columns[0]
+	dns_query_name = columns[3]
+
+
+
+#Hint: -e dns.qry.name is the key that will be common in the dns query and response so it can be used to match a query to its response 
