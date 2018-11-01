57a7785dffb8dc8bb80c07a0fd699318c2fd96f2
Talal Ahmad <mailtalalahmad@gmail.com>
2018-11-01 17:58:44 -0400

    file and folders added to dns file

--- dns-parser.py
+++ dns-parser.py
@@ -3,6 +3,11 @@ from subprocess import Popen, PIPE
 
 
 #-e dns.flags.rcode
+folders = 't-mobile_android  t-mobile_firefox  verizon_firefox  wired_android  wired_firefox'
+folders = folders.split() # folders variable is now an array that contains all folder names 
+files = 'adobe.com,cnn.com,qq.com,twitter.com,amazon.com,google.com,rakuten.co.jp,aol.com,microsoft.com,taobao.com'
+files = files.split(',')
+
 cmd = 'tshark -r ' + sys.argv[1] +' -2 -R "dns" -T fields -e frame.time_relative -e ip.src -e ip.dst -e dns.qry.name -e _ws.col.Info'
 
 print cmd
