19d0cb1cdcfc9ee12a8ef5e3036e3df6cfcbd70a
Talal Ahmad <mailtalalahmad@gmail.com>
2018-11-04 19:44:48 -0500

    basic dns graph works

--- dns-parser.py
+++ dns-parser.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import sys
 from subprocess import Popen, PIPE
 
+dnsqueries = {}
 
 #-e dns.flags.rcode
 folders = 't-mobile_android  t-mobile_firefox  verizon_firefox  wired_android  wired_firefox'
@@ -8,7 +10,9 @@ folders = folders.split() # folders variable is now an array that contains all f
 files = 'adobe.com,cnn.com,qq.com,twitter.com,amazon.com,google.com,rakuten.co.jp,aol.com,microsoft.com,taobao.com'
 files = files.split(',')
 
-cmd = 'tshark -r ' + sys.argv[1] +' -2 -R "dns" -T fields -e frame.time_relative -e ip.src -e ip.dst -e dns.qry.name -e dns.flags.rcode -e _ws.col.Info'
+current_file = "pcaps/"+folders[0]+"/*"+files[0]+"*.pcap"
+
+cmd = 'tshark -r ' + current_file +' -2 -R "dns" -T fields -e frame.time_relative -e ip.src -e ip.dst -e dns.qry.name -e dns.flags.rcode -e _ws.col.Info'
 
 print cmd
 
@@ -21,8 +25,58 @@ for line in lines:
 	print line
 	#this line seperates line into columns
 	columns = line.split("\t")
+	if columns == ['']:
+		continue
 	time = columns[0]
+	print columns
 	dns_query_name = columns[3]
+	dns_query_flag = columns[4]
+	if dns_query_flag == '':
+		if dns_query_name not in dnsqueries.keys():
+			dnsqueries[dns_query_name] = {"queries":[],"replies":[],"name":dns_query_name}
+			dnsqueries[dns_query_name]["queries"].append(time)
+		else:
+			dnsqueries[dns_query_name]["replies"].append((dns_query_flag,time))
+
+plt.xlabel('time (seconds)')
+
+yticks = []
+xpoints = []
+
+for dnsquery in dnsqueries:
+	#print dnsquery
+	yticks.append("DNSQUERY for "+ dnsquery)
+
+print yticks
+
+yticknums = range(len(yticks))
+
+yticknums.reverse()
+
+plt.yticks(yticknums,yticks)
+
+en = enumerate(dnsqueries.keys())
+for idx,key in en:
+	current_y = [len(dnsqueries) - idx -1]
+	querytimes = [float(x) for x in dnsqueries[key]["queries"]]
+	responsetimes = [float(x[1]) for x in dnsqueries[key]["replies"]]
+	responseflags = [x[0] for x in dnsqueries[key]["replies"]]
+
+	timepts = querytimes+responsetimes
+	plt.plot(timepts,current_y*len(timepts), "bo-")
+	plt.plot(responsetimes,current_y*len(responsetimes), "ro")
+
+	for idx, flag in enumerate(responseflags):
+		if flag==0:
+			plt.plot([responsetimes[idx]],current_y,"yo")
+
+plt.ylim(ymin=-1, ymax=len(yticks)+1)
+plt.show()
+
+
+
+
+
 
 
 
